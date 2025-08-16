010b35db7c0074d33d2bea6ff15c8a11c5995f90
Sihle Kambula <kambulasihle@gmail.com>
2025-08-16 22:21:05 +0200

    create icon constants for auth implementation

---
+++ lib/src/core/utils/icons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+import 'package:line_icons/line_icons.dart';
+
+class MyIcons {
+  IconData envelop = LineIcons.envelope;
+  IconData lock = LineIcons.lock;
+  IconData person = LineIcons.user;
+}
